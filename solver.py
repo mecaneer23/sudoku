a9fc93fa8d86f1245d2288656e934d55209aa5be
mecaneer23 <mecaneer23@gmail.com>
2023-02-24 08:32:54 -0600

    add helpful assert messages

--- solver.py
+++ solver.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 def validate_file(file):
-    lines = file.read().split("\n")
-    assert len(lines) == 9
-    for line in lines:
-        assert len(line.split(" ")) == 9
-        assert len(line) == 17, len(line)
+    lines = file.read().split("\n")[:-1]
+    assert len(lines) == 9, f"Board should have 9 lines, has {len(lines)}"
+    for i, line in enumerate(lines):
+        assert len(line.split(" ")) == 9, f"Line {i + 1} should have 9 characters, has {len(line.split(' '))}"
+        assert len(line) == 17, f"Make sure you have 9 space-seperated numbers. Currently, line {i + 1} has {len(line)} total characters, and should have 17."
     return lines
 
 
